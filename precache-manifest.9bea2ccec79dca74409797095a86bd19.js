915b0a967dd87c2443cd9136a8988793ca285d9e
rahul250892 <rahulkumar250892@outlook.com>
2020-02-28 00:09:03 +0530

    Updates

---
+++ precache-manifest.9bea2ccec79dca74409797095a86bd19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "231156873d5fc462dc4a",
+    "url": "/cartapplication/static/js/runtime~main.23115687.js"
+  },
+  {
+    "revision": "be4b82772acd5551e3da",
+    "url": "/cartapplication/static/js/main.be4b8277.chunk.js"
+  },
+  {
+    "revision": "377619c63333ef8da7ee",
+    "url": "/cartapplication/static/js/1.377619c6.chunk.js"
+  },
+  {
+    "revision": "be4b82772acd5551e3da",
+    "url": "/cartapplication/static/css/main.7b51a979.chunk.css"
+  },
+  {
+    "revision": "615296bd0cd1ea50e60190dfb1d502d9",
+    "url": "/cartapplication/index.html"
+  }
+];
